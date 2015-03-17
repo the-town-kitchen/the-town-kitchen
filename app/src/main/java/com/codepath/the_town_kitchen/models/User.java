4ff041b0fb9785d0d93f0e9cf10311da69323827
Xiangli Dai <xdai@yammer-inc.com>
2015-03-17 00:18:32 -0700

    saved user to both cloud and local on Parse

--- app/src/main/java/com/codepath/the_town_kitchen/models/User.java
+++ app/src/main/java/com/codepath/the_town_kitchen/models/User.java
@@ -1,12 +1,10 @@
 package com.codepath.the_town_kitchen.models;
 
-import com.parse.ParseClassName;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -46,34 +44,38 @@ public class User extends ParseObject {
     public void setFacebookId(String facebookId) { put("facebookId", facebookId); }
 
     public static User fromGooglePerson(Person person, GoogleApiClient mGoogleApiClient) {
-        User user = new User();
-        user.name = person.getDisplayName();
-        user.profileImageUrl = person.getImage().getUrl();
-        user.profileImageUrl = user.profileImageUrl.substring(0,
-                user.profileImageUrl.length() - 2)
+        ParseObject user = ParseObject.create("User");// new ParseObject("Message");
+        user.put("name", person.getDisplayName());
+        user.put("email", Plus.AccountApi.getAccountName(mGoogleApiClient));
+        String profileImageUrl = person.getImage().getUrl();
+        profileImageUrl =profileImageUrl.substring(0,
+                profileImageUrl.length() - 2)
                 + PROFILE_PIC_SIZE;
-        user.email = Plus.AccountApi.getAccountName(mGoogleApiClient);
-        try {
-            user.save();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return user;
+        user.put("profileImageUrl", profileImageUrl);
+        user.saveInBackground();
+        user.pinInBackground();
+        return (User) user;
+       // User user = new User();
+       // user.name = person.getDisplayName();
+
+
     }
 
     public static User fromFacebookJson(JSONObject json) {
-        User user = new User();
+        ParseObject user = ParseObject.create("User");// new ParseObject("Message");
+
         try {
-            user.email = json.getString("email");
-            user.name = json.getString("name");
-            user.facebookId = json.getString("id");
-            user.save();
-            return user;
+            user.put("name", json.getString("name"));
+            user.put("email", json.getString("email"));
+            user.put("facebookId", json.getString("id"));
+            user.saveInBackground();
+            user.pinInBackground();
+            return (User) user;
+
         } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
             e.printStackTrace();
         }
         return null;
     }
+
 }
